5e71a6dd95b00e1674c77bbf3bad3e239eb87a27
Courtney Tate <courtneytate@courtneys-mbp.lan>
2020-01-05 22:37:30 -0500

    updated bridal bios

--- src/pages/front/BridalParty/BridalParty.tsx
+++ src/pages/front/BridalParty/BridalParty.tsx
@@ -71,7 +71,12 @@ export const BridalParty = () => {
 							<PartyImage src={bridalParty.hayden} alt='Diana Koranda' />
 							<PartyName>Diana Koranda</PartyName>
 							<PartyTitle>Bridesmaid</PartyTitle>
-							<PartyCopy></PartyCopy>
+							<PartyCopy>
+								Diana and Courtney met through Dance Company and Chi Alpha Pi at
+								Augie. Through dance and friendship adventures, Court has found one
+								of the strongest leaders and best mama bears out that the world has
+								ever known.
+							</PartyCopy>
 						</PartyRightColumn>
 					</PartyRow>
 					<PartyRow>
@@ -86,9 +91,11 @@ export const BridalParty = () => {
 							<PartyName>Madeline Gorsuch</PartyName>
 							<PartyTitle>Bridesmaid</PartyTitle>
 							<PartyCopy>
-								Through Augustana greek life, Madeline and Courtney became sorority
-								sisters. Together they have been co-bigs, worked together in Dance
-								Marathon, and worked in Augie’s admissions office.{' '}
+								Through Augie greek life, Madeline and Courtney became sorority
+								sisters and such close friends. Together they have been co-bigs,
+								worked together in Dance Marathon, and worked in Augie’s admissions
+								office. This girl always has your back, and I have always been so
+								blessed to have her by my side.{' '}
 							</PartyCopy>
 						</PartyRightColumn>
 					</PartyRow>
@@ -105,11 +112,11 @@ export const BridalParty = () => {
 							<PartyTitle>Bridesmaid</PartyTitle>
 							<PartyCopy>
 								Courtney and Stef first met freshman year on the Augustana Vikettes,
-								but it wasn't until Courtney switched majors to communication
-								sciences and disorders that things fell more into place. They both
-								ended up going to Eastern Illinois University and rooming together
-								during grad school where they did everything together from being
-								workout buddiees to study buddies.
+								but it wasn't until grad school that they became buddies and
+								roommates at Eastern Illinois University. Stef and Court did
+								everything together from classes, graduate assistant offices next to
+								each other, study buddies, all the way to converting Courtney into a
+								workout buddy at 5am :)
 							</PartyCopy>
 						</PartyRightColumn>
 					</PartyRow>
@@ -124,7 +131,12 @@ export const BridalParty = () => {
 							<PartyImage src={bridalParty.hayden} alt='Blair Casarotto' />
 							<PartyName>Blair Casarotto</PartyName>
 							<PartyTitle>Bridesmaid</PartyTitle>
-							<PartyCopy></PartyCopy>
+							<PartyCopy>
+								Blair has been one of the best future sister-in-laws and roommates
+								that a girl could ask for. Not only is she one of the most caring
+								people we know, but she is a one of the most thoughtful friends a
+								girl could have.
+							</PartyCopy>
 						</PartyRightColumn>
 					</PartyRow>
 					<PartyRow>

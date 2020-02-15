93fb4935221cacf63e43ff6990957eeab97128ef
Stuart Casarotto <stuart.casarotto@38thstreetstudios.com>
2020-02-15 18:00:06 -0500

    v1 of timeline

--- src/pages/front/EventDetails/EventDetails.tsx
+++ src/pages/front/EventDetails/EventDetails.tsx
@@ -12,9 +12,38 @@ import {
 	LocationTitle,
 	LocationCopy,
 	LocationLink,
+	TimelineSection,
+	TimelineContainer,
+	TimelineLine,
+	TimelineRow,
+	TimelineLeftColumn,
+	TimelineCenterColumn,
+	TimelineRightColumn,
+	TimelineTimeTitle,
+	TimelineDot,
+	TimelineDotIcon,
+	TimelineRowTitle,
+	TimelineRowCopy,
 } from './styledComponents'
 import { PageWrapper } from 'components'
 import { eventDetails } from 'config/images'
+
+const timelineData = [
+	{ time: 'TBD', title: 'Hotel Check-In', copy: 'Freshen up and get ready for the big day.' },
+	{ time: 'TBD', title: 'Shuttle Pick-Up', copy: "Hop a bus ride so you don't have to drive" },
+	{
+		time: 'TBD',
+		title: 'Ceremony',
+		copy: 'Make yourselves comfortable and join us for a special moment',
+	},
+	{ time: 'TBD', title: 'Cocktail Hour', copy: 'Kick off the celebration and grab a drink' },
+	{ time: 'TBD', title: 'Reception', copy: 'Join us for dinner, drinks, speaches, and dacing' },
+	{
+		time: 'TBD',
+		title: 'Shuttle Return',
+		copy: 'Easy way to make it back at the end of the night',
+	},
+]
 
 export const EventDetails = () => {
 	return (
@@ -32,8 +61,8 @@ export const EventDetails = () => {
 					<LocationContent>
 						<LocationTitle>Ceremony and Reception</LocationTitle>
 						<LocationCopy>
-							Please join us on July 18th 2020 at DC Estate Winery 8877 State Line Rd,
-							South Beloit, IL 61080.
+							Please join us on Saturday, July 18th 2020 at DC Estate Winery 8877
+							State Line Rd, South Beloit, IL 61080.
 						</LocationCopy>
 						<LocationLink
 							href='https://www.dcestatewinery.com/'
@@ -52,6 +81,47 @@ export const EventDetails = () => {
 					</LocationContent>
 				</LocationContainer>
 			</LocationSection>
+			<TimelineSection>
+				<TimelineContainer>
+					<TimelineLine />
+					{timelineData.map((row, index) => {
+						const { time, title, copy } = row
+						return (
+							<TimelineRow key={index}>
+								<TimelineLeftColumn>
+									{index % 2 === 0 && (
+										<TimelineTimeTitle style={{ textAlign: 'right' }}>
+											{time}
+										</TimelineTimeTitle>
+									)}
+									{index % 2 === 1 && (
+										<>
+											<TimelineRowTitle>{title}</TimelineRowTitle>
+											<TimelineRowCopy>{copy}</TimelineRowCopy>
+										</>
+									)}
+								</TimelineLeftColumn>
+								<TimelineCenterColumn>
+									<TimelineDot>
+										<TimelineDotIcon />
+									</TimelineDot>
+								</TimelineCenterColumn>
+								<TimelineRightColumn>
+									{index % 2 === 1 && (
+										<TimelineTimeTitle>{time}</TimelineTimeTitle>
+									)}
+									{index % 2 === 0 && (
+										<>
+											<TimelineRowTitle>{title}</TimelineRowTitle>
+											<TimelineRowCopy>{copy}</TimelineRowCopy>
+										</>
+									)}
+								</TimelineRightColumn>
+							</TimelineRow>
+						)
+					})}
+				</TimelineContainer>
+			</TimelineSection>
 		</PageWrapper>
 	)
 }
--- src/pages/front/EventDetails/styledComponents.ts
+++ src/pages/front/EventDetails/styledComponents.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 import { pageHeaders } from 'config/images'
+import { HeartIcon } from 'assets/SVGs'
 
 export const Header = styled.div`
 	height: 60vh;
@@ -48,18 +49,17 @@ export const LocationContainer = styled.div`
 `
 export const LocationLogoWrapper = styled.div`
 	width: 50%;
-	padding-right: 15px;
+	padding-right: 20px;
 `
 export const LocationLogo = styled.img`
 	display: block;
 	width: 100%;
-	max-width: 500px;
 	margin: 0px auto;
 	border-radius: 10px;
 `
 export const LocationContent = styled.div`
 	width: 50%;
-	padding-left: 15px;
+	padding-left: 20px;
 `
 export const LocationTitle = styled.h2`
 	margin-top: 0px;
@@ -74,7 +74,6 @@ export const LocationCopy = styled.p`
 	margin-top: 0px;
 	margin-bottom: 15px;
 	color: ${(props) => props.theme.darkerGray};
-	max-width: 475px;
 `
 export const LocationLink = styled.a`
 	display: inline-block;
@@ -92,4 +91,80 @@ export const LocationLink = styled.a`
 		color: ${(props) => props.theme.white};
 		background-color: ${(props) => props.theme.primary};
 	}
+`
+
+export const TimelineSection = styled.div`
+	padding-top: 50px;
+	padding-bottom: 80px;
+`
+export const TimelineContainer = styled.div`
+	max-width: 1000px;
+	margin: 0px auto;
+	padding-left: 30px;
+	padding-right: 30px;
+	position: relative;
+`
+export const TimelineLine = styled.div`
+	position: absolute;
+	left: calc(50% - 2px);
+	height: 100%;
+	width: 4px;
+	background-color: ${(props) => props.theme.primary};
+	z-index: 1;
+`
+export const TimelineRow = styled.div`
+	display: flex;
+	justify-content: center;
+	width: 100%;
+	padding-top: 30px;
+	padding-bottom: 50px;
+`
+export const TimelineLeftColumn = styled.div`
+	flex: 1;
+	padding-right: 20px;
+	padding-top: 20px;
+`
+export const TimelineCenterColumn = styled.div`
+	width: 40px;
+	flex-shrink: 0;
+`
+export const TimelineRightColumn = styled.div`
+	flex: 1;
+	padding-left: 20px;
+	padding-top: 20px;
+`
+export const TimelineTimeTitle = styled.h3`
+	margin-top: 0px;
+	margin-bottom: 0px;
+	font-size: 36px;
+	color: ${(props) => props.theme.primary};
+`
+export const TimelineDot = styled.div`
+	border-radius: 50%;
+	width: 100%;
+	background-color: ${(props) => props.theme.primary};
+	padding: 10px;
+	z-index: 5;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
+export const TimelineDotIcon = styled(HeartIcon)`
+	width: 100%;
+	fill: ${(props) => props.theme.white};
+	z-index: 5;
+`
+export const TimelineRowTitle = styled.h3`
+	margin-top: 0px;
+	margin-bottom: 10px;
+	font-size: 42px;
+	font-family: 'Great Vibes', cursive;
+	color: ${(props) => props.theme.primary};
+`
+export const TimelineRowCopy = styled.p`
+	margin-top: 0px;
+	margin-bottom: 10px;
+	line-height: 1.5;
+	font-size: 20px;
+	color: ${(props) => props.theme.darkerGray};
 `
